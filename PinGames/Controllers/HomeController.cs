23fe518f3ab3a0ddb43b596a789c7ee2587b4959
Fanrell <cieslik.kamil@outlook.com>
2021-03-30 22:56:27 +0200

    Login and Register Pages

--- PinGames/Controllers/HomeController.cs
+++ PinGames/Controllers/HomeController.cs
@@ -33,5 +33,14 @@ namespace PinGames.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpGet]
+        public IActionResult LoginAction()
+        {
+            return View("Privacy");
+        }
+        public IActionResult RegisterAction()
+        {
+            return View("Register");
+        }
     }
 }
--- PinGames/Startup.cs
+++ PinGames/Startup.cs
@@ -24,6 +24,7 @@ namespace PinGames
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,8 @@ namespace PinGames
             app.UseRouting();
 
             app.UseAuthorization();
+
+            
 
             app.UseEndpoints(endpoints =>
             {
